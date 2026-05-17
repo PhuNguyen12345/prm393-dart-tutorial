c0dd6755f73c00d3ac721d703d0e030bdd14c99b
PhuNguyen12345 <phukak12345@gmail.com>
2026-05-17 10:12:15 +0700

    initial commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,19 @@
+import 'package:cli/cli.dart' as cli;
+
+const version = "0.0.1";
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print("Hello, Dart!");
+  } else if (arguments.first == "version") {
+    print("Dartpedia CLI version $version");
+  }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+
