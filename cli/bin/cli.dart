b96dc249fccbda6f10365e9de7b84ef81ea9ee1b
PhuNguyen12345 <phukak12345@gmail.com>
2026-05-17 10:35:02 +0700

    feat: implement search command.

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -4,13 +4,14 @@ const version = "0.0.1";
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); 
-  }
-  else if (arguments.first == "version") {
-    print("Dartpedia CLI version $version"); 
-  }
-  else {
-    printUsage(); 
+    printUsage();
+  } else if (arguments.first == "version") {
+    print("Dartpedia CLI version $version");
+  } else if (arguments.first == "search") {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
   }
 }
 
@@ -20,4 +21,7 @@ void printUsage() {
   );
 }
 
-
+//List<String>? arguments means arguments list itself can be null
+void searchWikipedia(List<String>? arguments) {
+  print("SearchWikipedia received arguments: $arguments");
+}
