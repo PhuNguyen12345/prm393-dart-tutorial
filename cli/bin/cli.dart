6194df2327b9d2735ad59acfa14f84a2a2059cff
PhuNguyen12345 <phukak12345@gmail.com>
2026-05-17 10:16:34 +0700

    feat: version and help commands

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -3,10 +3,14 @@ import 'package:cli/cli.dart' as cli;
 const version = "0.0.1";
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print("Hello, Dart!");
-  } else if (arguments.first == "version") {
-    print("Dartpedia CLI version $version");
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage(); 
+  }
+  else if (arguments.first == "version") {
+    print("Dartpedia CLI version $version"); 
+  }
+  else {
+    printUsage(); 
   }
 }
 
